d5d1f18fc1908756838eefee1d3bdd9769ecbff3
Leonardo Salles <lsleonardosalles.999@gmail.com>
2024-09-02 16:35:40 -0300

    ...

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { Estilos } from "./componentes/EstilosGlobais/Estilos";
-
+import { Card } from "./componentes/Card/Card";
+import { ProvedorTema } from "./componentes/ProvedorTema/ProvedorTema";
 
 function App() {
   return (
-    <div>
+    <ProvedorTema>
       <Estilos />
-      <h1>Freelando</h1>
-    </div>
+      <Card>
+        <h1>Freelando</h1>
+      </Card>
+
+    </ProvedorTema>
   );
 }
 
